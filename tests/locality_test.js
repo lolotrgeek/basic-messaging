badfa0cef56e3c20e563c4bbf405658c9fc3fa99
lolotrgeek <jon@simplyenable.com>
2022-05-18 09:05:16 -0500

    test: locality

--- tests/locality_test.js
+++ tests/locality_test.js
@@ -1,3 +1,15 @@
-// spawn 10 nodes, they ping for neighbors and each build a neighbor graph
+// spawn lots of nodes, they ping for neighbors and each build a neighbor graph
+const path = require('path')
+const fs = require('fs')
+const { fork } = require('child_process')
 
-
+const file = "/one_test.js"
+const directoryPath = path.join(__dirname)
+const nodes = 200
+let count = 0
+while (count < nodes) {
+    if (count >= nodes) break
+    let child = fork(directoryPath + file, { stdio: ['ignore', 'ignore', 'ignore', 'ipc'] })
+    // console.log(count, child)
+    count++
+}
