72fde45f09a8ff145dc2faec171a00a1089459fd
lolotrgeek <jon@simplyenable.com>
2022-05-31 08:04:28 -0500

    test: ping locality

--- tests/locality_test.js
+++ tests/locality_test.js
@@ -3,13 +3,14 @@ const path = require('path')
 const fs = require('fs')
 const { fork } = require('child_process')
 
-const file = "/one_test.js"
+const file = "/ping_test.js"
 const directoryPath = path.join(__dirname)
-const nodes = 200
+const nodes = 10
 let count = 0
 while (count < nodes) {
     if (count >= nodes) break
     let child = fork(directoryPath + file, { stdio: ['ignore', 'ignore', 'ignore', 'ipc'] })
-    // console.log(count, child)
+    // console.log(child)
+    console.log(count)
     count++
 }
--- tests/ping_test.js
+++ tests/ping_test.js
@@ -1,4 +1,4 @@
 const { Node } = require("../main")
 
 let pinger = new Node()
-setInterval(() => pinger.ping_all() ,5000)
+setInterval(() => pinger.ping_all() ,2000)
