cdaf651eaadb6c6b08f5bf3c0e3d7bd61e0e3eff
lolotrgeek <jon@simplyenable.com>
2022-06-04 07:01:05 -0500

    test: updates

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,3 @@
-const { Node } = require("../main")
+const { spawn_node } = require("../main")
 
-const node  = new Node()
-const node2  = new Node()
-
-const heard = message => console.log(`Heard: ${message}`)
-
-node.listen("test", heard)
-node.listen("hello", heard)
-
-setTimeout(() => node2.send("test", "testing"), 500)
-setTimeout(() => node2.send("hello", "world"), 500)
+spawn_node(5)
