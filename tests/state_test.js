d6516b81e7fdc2bec6caf0fa8b37dd10398305e1
lolotrgeek <jon@simplyenable.com>
2022-06-26 12:53:04 -0500

    test: state as grid

---
+++ tests/state_test.js
@@ -0,0 +1,43 @@
+const { Node } = require("../src/node")
+const { table } = require('table')
+const node = new Node("state_test")
+
+console.clear()
+
+let headers = ["Chain", "Peer", "State", "Type"]
+let peers = [headers]
+
+function addPeer(name) {
+    let found = peers.find(peer => peer[1] === name)
+    console.clear()
+    console.log(table(peers))
+    if (!found) {
+        let row = ["", name, -1, 'number']
+        peers.push(row)
+        node.listen(name, (message, from) => {
+            let data = JSON.parse(message)
+            if (data.state) {
+                let found = peers.findIndex(peer => peer[1] === from)
+                
+                peers[found] = [data.chain_id, from, data.state, typeof data.state]
+                console.clear()
+                console.log(table(peers))                
+            }
+        })
+    }
+}
+
+function removePeer(name) {
+    try {
+        // node.core.removeListener(name)
+        let found = peers.findIndex(peer => peer[1] === name)
+        peers.splice(found, 1)
+        console.clear()
+        console.log(table(peers))       
+    } catch (error) {
+        
+    }
+}
+
+node.core.on("connect", (id, name) => addPeer(name))
+node.core.on("disconnect", (id, name) => removePeer(name))
