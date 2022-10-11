19e48c7b009de690cc717cf9d94670b304c4fc01
lolotrgeek <jon@simplyenable.com>
2022-10-11 16:26:27 -0500

    chore: merge tests

--- src/network.js
+++ src/network.js
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-const zmq = require("zeromq")
-
-class Node {
-    constructor(name) {
-        if (!name) throw "Must pass a name to Node!"
-        this.debug = false
-        this.port = 3000
-        this.base = "tcp://127.0.0.1:"
-        this.address = this.base + this.port
-        this.publisher
-        this.subscriber = new zmq.Subscriber
-    }
-
-    decode(data) {
-        try { return JSON.parse(data) } catch (error) { return data }
-    }
-
-    encode(data) {
-        return JSON.stringify(data)
-    }
-
-    /**
-     * 
-     * @param {string | object} channel name of channel or `{to: id}`
-     * @param {*} message 
-     */
-    send(channel, message, options) {
-        return new Promise(async resolve => {
-            try {
-                if(!this.publisher) {
-                    this.publisher = new zmq.Publisher
-                    await this.publisher.bind(this.address)
-                }
-                console.log("sending", message)
-                return resolve(await this.publisher.send([channel, this.encode(message)]))
-            } catch (error) {
-                console.log("Caught:" , error)
-                this.port+1
-                resolve(await this.send(channel, message, options))
-            }
-        })
-    }
-
-    /**
-     * 
-     * @param {string} channel 
-     * @param {function} listener `(msg: string)`
-     */
-    async listen(channel, listener) {
-
-        this.subscriber.connect(this.address)
-        this.subscriber.subscribe(channel)
-
-        for await (const [topic, msg] of this.subscriber) {
-            listener(this.decode(msg))
-            // console.log("received a message related to:", topic, "containing message:", msg)
-        }
-
-    }
-=======
 const { Subscriber, Publisher } = require('cote')
 const { randomUUID } = require('crypto')
 
@@ -94,7 +32,6 @@ class Node {
         this.core.publish(channel, message)
     }
 
->>>>>>> basic-cote
 }
 
 
--- tests/group_test.js
+++ tests/group_test.js
@@ -1,18 +1,4 @@
 const { Node } = require("../main")
-<<<<<<< HEAD
-const node = new Node("test")
-const node2 = new Node("test2")
-node.debug = 'join'
-node2.debug = 'join'
-
-const test = message => {
-    console.log("Passed:", typeof message === 'object' && Object.values(message)[0].groups[0] === 'test')
-    if(process) process.exit() 
-}
-
-node.join("test").then(console.log)
-node2.join("test").then(test)
-=======
 
 const node  = new Node()
 const node2  = new Node()
@@ -29,4 +15,3 @@ setInterval(() => {
     node.send("hello", "world")
 }, 2000)
 //fails if we do not hear "world"
->>>>>>> basic-cote
